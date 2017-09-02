cf807278adcd9955421755d8624ce7ff9ce53f0a
Tsur Herman <tsur@augmedics.com>
2017-09-02 05:03:13 +0300

    dynamic base

--- src/pcore.jl
+++ src/pcore.jl
@@ -1,26 +1,39 @@
 
-mutable struct Signal{T}
-    data::T
+mutable struct Signal
+    data
     action::Function
     children::Vector{Signal}
+    Signal(data,f::Function) = new(data,f,Signal[])
 end
 
-@inline value(s::Signal) = s.data
-@inline value(s::T) where T = s
-@inline action(s) = s.action()
+function value(s::Signal)
+    s.data
+end
+function value(s::T) where T
+    s
+end
+function action(s::Signal)
+    s.action
+end
+function _call(f::Function)
+    f()
+end
+function set_value!(s::Signal,d)
+     s.data = d
+end
 
-@inline (s::Signal{T})(val) where T = begin
-    s.data = convert(T,val)
+(s::Signal)(val)  = begin
+    set_value!(s,val)
     for child in s.children
-        child.data = child.action()
+        set_value!(child,_call(action(child)))
     end
 end
 
 
-Signal(val) = Signal(val,() -> nothing,Vector{Signal}())
-Signal(f,arg1,arg2...) = begin
+Signal(val) = Signal(val,() -> nothing)
+Signal(f::Function,arg1,arg2...) = begin
     args = (arg1,arg2...)
-    s = Signal(f(value.(args)...),() -> f(value.(args)...),Vector{Signal}())
+    s = Signal(f(value.(args)...),() -> f(value.(args)...))
     for arg in args
         isa(arg,Signal) && push!(arg.children,s)
     end
@@ -32,16 +45,4 @@ B = Signal(@SMatrix rand(4,4))
 C = Signal(A,B) do a,b
     a*b
 end
-
-
-
-function maps(f,input,inputs...)
-    vargs = (input,inputs...)
-    const args = data.(vargs)
-    const ref = Ref(f(data.(args)...))
-    return () -> ref.x = convert(typeof(ref.x),f(data.(args)...))
-end
-
-C = maps(A,B) do a,b
-    a+b
-end
+Z = @SMatrix zeros(4,4)
