e947d64d2507bc17c28aed1959cef07ce0808467
Tsur Herman <tsur@augmedics.com>
2017-09-05 22:31:51 +0300

    organized added types

--- src/pcore.jl
+++ src/pcore.jl
@@ -1,78 +1,65 @@
 
-mutable struct Signal
+function nop()
+    nothing
+end
+mutable struct Signal{F,ARGS}
     data
-    action::Function
-    poll_action::Function
+    f::F
+    args::ARGS
     children::Vector{Signal}
-
     data_age::Int
 end
 
-create_action(f::Function,args::Tuple,trans::Function = x->x) = begin
-    () -> try f(trans.(args)...) catch error("blah"); end
+Signal(val) = begin
+    s = Signal(val,nop,(),Signal[],0)
 end
 
-(s::Signal)()  = begin
-    s.data
-end
-
-function value(s::Signal)
-    s()
-end
-
-function value(s::T) where T
+Signal(f::Function,args...) = begin
+    s = Signal(nothing,f,args,Signal[],0)
+    for arg in args
+        isa(arg,Signal) && push!(arg.children,s)
+    end
     s
 end
 
+function value(s::Signal)
+    s.data
+end
+value(s) = s
+
+import Base.getindex
+function getindex(s::Signal)
+    value(s)
+end
 
 function set_value!(s::Signal,val)
     s.data_age += 1
     s.data = val
 end
 
-function update!(s::Signal)
-    set_value!(s,s.action())
-    foreach(update!,s.children)
-end
-
-function poll!(s::Signal)
-    s.data = s.poll_action()
-end
-
-function poll!(s::T) where T
-    s
-end
-
-(s::Signal)(val)  = begin
+#push!
+(s::Signal)(val) = push!(s,val)
+import Base.push!
+function push!(s::Signal,val)
     set_value!(s,val)
-    foreach(update!,s.children)
+    foreach(push!,s.children)
 end
 
-#base_construc
-Signal(data,action::Function,poll_action::Function) = begin
-    s = Signal(data,action,poll_action,Signal[],0)
-    finalizer(s,(s) -> @async println("finialized Signal"))
-    s
+function push!(s::Signal)
+    push!(s,action_on_value(s))
 end
 
-Signal(val) = begin
-    nop = ()->nothing
-    s = Signal(val,nop,nop);
-    data = create_action(()->s.data,())
-    s.action = data
-    s.poll_action = data
-    s
+function action_on_value(s::Signal)
+    s.f(map(value,s.args)...)
 end
 
-Signal(f::Function,args...) = begin
-    g = () -> f(map(value,args)...)
-    poll_action = () -> begin
-        foreach(poll!,args);
-        action()
-    end
-    s = Signal(g(),g,poll_action)
-    for arg in args
-        isa(arg,Signal) && push!(arg.children,s)
-    end
-    s
+#pull!
+function pull!(s::Signal)
+    set_value!(s,action_on_pull(s))
+    value(s)
+end
+pull!(s) = s
+
+function action_on_pull(s::Signal)
+    s.f(map(pull!,s.args)...)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,32 +1,28 @@
-using Proactive: Signal
+using Proactive: Signal,poll!
 using Base.Test
 
-# write your own tests here
-# @test 1
 # typ = Matrix
 typ = SMatrix{4,4,Float64,16}
-
-A = Signal(typ(rand(4,4)))
-B = Signal(typ(rand(4,4)))
-
-C = Signal(typ(rand(4,4)))
-D = Signal(typ(rand(4,4)))
-
-E = Signal(A,B) do a,b
-    a*b
+base_n = 4
+n = 24
+RNG = srand(1234567)
+signals = Signal[Signal(typ(rand(RNG,4,4))) for i=1:base_n]
+for i=1:n
+    A = rand(RNG,signals)
+    B = rand(RNG,signals)
+    E = Signal(A,B) do a,b
+        a*b
+    end
+    push!(signals,E)
 end
-F = Signal(C,D) do a,b
-    a*b
-end
-G = Signal(E,F) do e,f
-    e*f
-end
+
 Z = typ(zeros(4,4))
-A(Z)
+A = signals[2]
+G = signals[end]
 
 @benchmark $A($Z)
 
-poll!(G)
+@benchmark pull!(G)
 
 
 
@@ -34,27 +30,26 @@ poll!(G)
 using Reactive
 Reactive.async_mode.x = false
 
-typ = Matrix
-
-A = Signal(typ(rand(4,4)))
-B = Signal(typ(rand(4,4)))
-
-C = Signal(typ(rand(4,4)))
-D = Signal(typ(rand(4,4)))
-
-E = map(A,B) do a,b
-    a*b
+typ = SMatrix{4,4,Float64,16}
+base_n = 4
+n = 24
+RNG = srand(1234567)
+signals = Signal[Signal(typ(rand(RNG,4,4))) for i=1:base_n]
+for i=1:n
+    A = rand(RNG,signals)
+    B = rand(RNG,signals)
+    E = map(A,B) do a,b
+        a*b
+    end
+    push!(signals,E)
 end
-F = map(C,D) do a,b
-    a*b
-end
-G = map(E,F) do e,f
-    e*f
-end
+
 Z = typ(zeros(4,4))
-push!(A,Z)
+A = signals[1]
+G = signals[end]
 
-@benchmark push!(A,Z)
+@benchmark push!($A,$Z)
+
 
 
 
